cd1746fa7178061e13faba291f6df130e5d1eb8d
Ricardo <ricardo.gon.tell@gmail.com>
2013-12-27 02:19:01 -0600

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewsTeller::Application.config.secret_key_base = 'b9396ec3bf52d2e6c785872d9a45b8a04a67c30e361019b6a5bc06c2b613c29a9c36a159366561ccc5eef57105fac479e859526cb7b8d95acf1b31667ea4ef80'
